2ba57933ac9d50bbc68b1cd34095daa4123e89a3
root <djelloulrouba@yahoo.com>
2025-01-17 09:57:15 +0000

    Script convertn loser in uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def uppercase(str):
+    for lettre in str:
+        if ord(lettre) >= 97 and ord(lettre) <= 122:
+            lettre = chr(ord(lettre) - 32)
+        print(f"{lettre}", end= "")
+    print("")
