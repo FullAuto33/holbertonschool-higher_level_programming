911c4b8ae24cd54eaea953d03da8564fbb0b1317
root <djelloulrouba@yahoo.com>
2025-01-17 09:58:08 +0000

    fix problem to convert format

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -4,5 +4,5 @@ def uppercase(str):
     for lettre in str:
         if ord(lettre) >= 97 and ord(lettre) <= 122:
             lettre = chr(ord(lettre) - 32)
-        print(f"{lettre}", end= "")
+        print("{}".format(lettre), end= "")
     print("")
