f96ca873e820348ca930f8b55c9e82047d9a6eaa
root <djelloulrouba@yahoo.com>
2025-01-17 09:40:53 +0000

    Script print combinaison for number

---
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for numero1 in range(0, 10):
+    for numero2 in range(numero1 + 1, 10):
+        if numero1 != 8 or numero2 != 9:
+            print(f"{numero1}{numero2}", end=", ")
+        else:
+            print(f"{numero1}{numero2}")
