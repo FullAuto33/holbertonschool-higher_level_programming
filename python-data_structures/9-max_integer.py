d5140473774eb06fd89e145e44cd3f0ede6fd6ec
root <djelloulrouba@yahoo.com>
2025-01-23 14:23:13 +0000

    Script print max integer in list

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    taille = len(my_list)
+    if not size:
+        return None
+    else:
+        compteur = my_list[0]
+        for i in my_list[1:]:
+            if i > compteur:
+                compteur = i
+        return compteur
