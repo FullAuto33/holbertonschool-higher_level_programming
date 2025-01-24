9a1533b0e89287cb7319d72de26953c3e24132c5
root <djelloulrouba@yahoo.com>
2025-01-24 09:59:58 +0000

    Script fix problem to style code with many line

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
 def carre(x):
     return (x**2)
+
+
 def square_matrix_simple(matrix=[]):
     result = matrix[:]
     for i in range(len(matrix)):
         for j in range(len(matrix[i])):
             result[i] = list(map(carre, matrix[i]))
-    return result
+    return (result)
