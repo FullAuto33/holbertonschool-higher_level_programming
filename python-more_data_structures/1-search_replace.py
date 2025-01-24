b6cf47e8e28b0d844cd8c55412a503dc944260fc
root <djelloulrouba@yahoo.com>
2025-01-24 10:13:09 +0000

    Script fix problem to = no two ==

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -3,5 +3,5 @@ def search_replace(my_list, search, replace):
     result = my_list[:]
     for i in range(len(my_list)):
         if my_list[i] == search:
-            result[i] == replace
+            result[i] = replace
     return result
