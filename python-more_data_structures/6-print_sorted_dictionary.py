24ad1e23d45eb7d0f71d50bc8fa9ec6eeb6b9f69
root <djelloulrouba@yahoo.com>
2025-01-26 10:40:19 +0000

    Fix problem to variable

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
     for i in sorted(a_dictionary):
-        result = a_dictionary.get(key)
+        result = a_dictionary.get(i)
         print('{}: {}'.format(i, result))
