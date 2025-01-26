01e69552ca1f9c090570be467aa9dcca4621ba59
root <djelloulrouba@yahoo.com>
2025-01-26 19:24:47 +0000

    Script update fix problem

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary is not None:
-        return (max(a_dictionary))
-    else:
-        return None
+        result = len(a_dictionary.values())
+        if result > 0:
+            count = max(a_dictionary.values())
+            for i in a_dictionary.keys():
+                if a_dictionary[i] == count:
+                    return i
+    return None
