2a3b0d2b588a7ff4e411cdd499999394214e1588
root <djelloulrouba@yahoo.com>
2025-01-26 12:02:00 +0000

    Fix problem to varibale with result

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     result = a_dictionary.copy()
-    for i in new_dict.keys():
+    for i in result.keys():
         result[i] *= 2
     return result
