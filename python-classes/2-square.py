498b1232b8df85099391f81a0a5ea40e723057bd
root <djelloulrouba@yahoo.com>
2025-01-30 17:55:46 +0000

    Script fix problem if condition if not instance integer

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -4,9 +4,9 @@
 
 class Square:
     """class Square size"""
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        if type(size) != int:
-            raise TypeError("size must be an integer")
         self.__size = size
