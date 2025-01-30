79c12a3d2032179ccce9a25738f1d43de8153ae8
root <djelloulrouba@yahoo.com>
2025-01-30 17:52:26 +0000

    Script define size of square

---
+++ python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Description module empty classes"""
+
+
+class Square:
+    """class Square size"""
+    def __init__(self, size = 0):
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        self.__size = size
