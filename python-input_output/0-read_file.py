23e32deead13a8c993168cefcc040bee3129e716
root <djelloulrouba@yahoo.com>
2025-02-11 09:39:39 +0000

    Scrpt read the file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+""" Read text file UTF"""
+def read_file(filename=""):
+    """Print content of file"""
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
