598404df6d06e0a7a5abb62407365d9210692429
root <djelloulrouba@yahoo.com>
2025-02-12 09:38:02 +0000

    Script write in UTF8

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defines write file in UTF8"""
+
+def write_file(filename="", text=""):
+    """Function write in file"""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
