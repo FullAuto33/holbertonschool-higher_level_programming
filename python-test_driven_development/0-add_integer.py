7f1b29c5108d833adb2f63e5555dfb54a35809aa
Djelloul rouba <djelloulrouba@yahoo.com>
2025-04-17 10:55:04 +0200

    Update script add space

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def add_integer(a, b=98):
     if a not isinstance(int) or not isinstance(a, float):
-raise TypeError("a must be an integer")
+        raise TypeError("a must be an integer")
     if b not isinstance(int) or not isinstance(b, float):
-raise TypeError("b must be an integer")
+        raise TypeError("b must be an integer")
     if isinstance(a, float):
         a = int(a)
     if isinstance(b, float):
