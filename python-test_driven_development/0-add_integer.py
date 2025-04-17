fac7b4fb4888be2b27503a44fb2fb14d9fdc69bb
Djelloul rouba <djelloulrouba@yahoo.com>
2025-04-17 11:20:03 +0200

    Script add document the function

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -3,6 +3,9 @@
 Module to add two integers.
 """
 def add_integer(a, b=98):
+    """
+    Function that adds two integers or floats.
+    """
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
