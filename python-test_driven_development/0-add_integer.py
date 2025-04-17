e252597e30af36fc93621f03862b8f7f4c32bcaf
Djelloul rouba <djelloulrouba@yahoo.com>
2025-04-17 11:18:47 +0200

    Script add module in script file

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+Module to add two integers.
+"""
 def add_integer(a, b=98):
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
