f92f02ef16f84738287bbff0514f9324ac1b985c
Djelloul rouba <djelloulrouba@yahoo.com>
2025-04-17 10:43:18 +0200

    Script add two integer

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def add_integer(a, b=98):
+    if a not isinstance(int) or not isinstance(a, float):
+raise TypeError("a must be an integer")
+    if b not isinstance(int) or not isinstance(b, float):
+raise TypeError("b must be an integer")
+    if isinstance(a, float):
+        a = int(a)
+    if isinstance(b, float):
+        b = int(b)
+    return a + b
