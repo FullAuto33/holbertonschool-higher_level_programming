04958c3bcc6115f5be8533489d5e8a1f3472aa7c
root <djelloulrouba@yahoo.com>
2025-05-06 08:11:52 +0000

    Script print value of variable a

---
+++ python-import_modules/5-variable_load.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """ Print value of variable"""
+    from variable_load_5 import a  # Import le module variable_load_5
+    print(a)  # Affiche la valeur de la variable a
