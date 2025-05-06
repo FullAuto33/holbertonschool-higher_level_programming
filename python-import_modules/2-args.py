0edea01a5de7f557603a724d009039ede46422a1
root <djelloulrouba@yahoo.com>
2025-05-06 07:51:51 +0000

    Script update to return argument and values with documentation of code

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     """ Print the number of arguments and their values"""
-    import sys
-    count = len(sys.argv) - 1
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(count))
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    import sys # Import la bibliothèque sys
+    count = len(sys.argv) - 1 # compte le nombre d'arguments
+    if count == 0: # Si le nombre d'arguments est égal à 0
+        print("0 arguments.") # Affiche 0 arguments
+    elif count == 1: # Si le nombre d'arguments est égal à 1
+        print("1 argument:") # Affiche 1 argument
+    else: # Si le nombre d'arguments est supérieur à 1
+        print("{} arguments:".format(count)) # Affiche le nombre d'arguments
+    for i in range(count): # Pour chaque argument
+        print("{}: {}".format(i + 1, sys.argv[i + 1])) # Affiche la position et la valeur de l'argument
