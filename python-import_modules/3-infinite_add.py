bf69002a958687cfc85ddbd1695bce5a265ea34f
root <djelloulrouba@yahoo.com>
2025-05-06 08:03:24 +0000

    Update script to import argv only

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     """ Print result of the addition of all arguments"""
-    import sys  # Import la bibliothèque sys
+    from sys import argv  # Import la bibliothèque sys
     result = 0  # Initialise le résultat à 0
-    for i in range(1, len(sys.argv)):  # Pour chaque argument
-        result += int(sys.argv[i])  # Ajoute l'argument à la somme
+    for i in range(1, len(argv)):  # Pour chaque argument
+        result += int(argv[i])  # Ajoute l'argument à la somme
     print("{}".format(result))  # Affiche le résultat
