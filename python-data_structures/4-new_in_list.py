e240044cb3e43e542442e7b07ef68d2c624ecaae
root <djelloulrouba@yahoo.com>
2025-05-12 08:07:43 +0000

    Script update with fix probelm to pycodestyle

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    """Replace an element in a list at a specific index without modifying the original list."""
+    """Replace an element in a list."""
     if idx < 0 or idx >= len(my_list):
         return my_list.copy()
     new_list = my_list.copy()
