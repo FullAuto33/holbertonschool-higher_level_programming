644d27e279240f000e5a5afbf552ace5e37cee6d
root <djelloulrouba@yahoo.com>
2025-05-12 07:44:05 +0000

    Update fix problem to pycodestyle with commentary

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -2,5 +2,5 @@
 
 def print_list_integer(my_list=[]):
     """Prints all integers of a list."""
-    for i in my_list: #pour chaque element de my_list
-        print("{:d}".format(i)) #affiche l'element i
+    for i in my_list:  # Pour chaque element de my_list
+        print("{:d}".format(i))  # Affiche l'element i
