6c3798971c7aa3048e00e9f7e4850f83904f35a5
root <djelloulrouba@yahoo.com>
2025-05-12 09:07:20 +0000

    Script print integer in matrix

---
+++ python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    """Prints a matrix of integers."""
+    for ligne in matrix:  # Pour chaque ligne dans la matrice
+        for colone in ligne:  # Pour chaque colonne dans la ligne
+            if colone != ligne[-1]:
+                # Si ce n'est pas le dernier élément de la ligne
+                print("{:d}".format(colone), end=" ")
+                # Affiche l'élément avec un espace
+            else:  # Si c'est le dernier élément de la ligne
+                print("{:d}".format(colone), end="\n")
+                # Affiche l'élément sans espace
