140ed3d04c95b4e2d53ffbbb575f2637180a98c8
root <djelloulrouba@yahoo.com>
2025-05-13 13:54:39 +0000

    Script update to use map and lambda for multiplied number

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -2,8 +2,5 @@
 
 def multiply_list_map(my_list=[], number=0):
     """Returns a list with all values multiplied by a number."""
-    result = []  # On initialise une liste vide
-    for i in range(len(my_list)):  # On parcourt la liste
-        result.append(my_list[i] * number)
-        # On ajoute le resultat de la multiplication à la liste
-    return result  # On retourne la liste
+    return list(map(lambda x: x * number, my_list))
+    # On retourne une liste avec les valeurs multipliées par le nombre
