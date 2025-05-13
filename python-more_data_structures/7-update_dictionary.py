e5b3bf77835b9d7e00966cc745e5cd897a43f43c
root <djelloulrouba@yahoo.com>
2025-05-13 13:29:55 +0000

    Script return new dictionary woth new value or new key

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    """Replace or add key/value in a dictionary."""
+    a_dictionary[key] = value
+    # Remplace ou ajoute la clé et sa valeur dans le dictionnaire
+    return a_dictionary  # Retourne le dictionnaire mis à jour
