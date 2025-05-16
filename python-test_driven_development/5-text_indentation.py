8034acc26498777819d4dd219bb7b947950c4a1c
root <you@example.com>
2025-05-16 15:27:09 +0000

    Script update to remove space after specify chr

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -6,9 +6,14 @@ def text_indentation(text):
     """ prints a text with 2 new lines after characters: ., ? and :"""
     if not isinstance(text, str):  # Verifie si text est une chaîne
         raise TypeError("text must be a string")  # Retourne TypeError
-    new_text = ""  # Initialise une nouvelle chaîne
-    for i in range(len(text)):  # Pour chaque caractère de la chaîne
-        new_text += text[i]  # Ajoute le caractère à la nouvelle chaîne
-        if text[i] in ".?:":  # Si le caractère est ., ? ou :
-            new_text += "\n\n"  # Ajoute deux nouvelles lignes
-    print(new_text.strip(), end="")  # Affiche la nouvelle chaîne sans espaces vides
+    compteur = 0
+    while compteur < len(text):
+        print(text[compteur], end="")
+        if text[compteur] in ".?:":
+            print("\n")
+            compteur += 1
+            # Sauter les espaces après la ponctuation
+            while compteur < len(text) and text[compteur] == " ":
+                compteur += 1
+            continue
+        compteur += 1
