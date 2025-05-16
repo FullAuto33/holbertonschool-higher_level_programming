095ff6ec5a1ce14f29832a33ea540e38e3483673
root <djelloulrouba@yahoo.com>
2025-05-16 07:55:37 +0000

    Script add two integer not test

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def add_integer(a, b=98):
+    """ Retourne la somme de deux entier"""
+    if type(a) not in [int, float]:  # Si a n'est pas un entier
+        raise TypeError("a must be an integer")
+        # Retourne message d'erreur
+    if type(b) not in [int, float]:  # Si b n'est pas un entier
+        raise TypeError("b must be an integer")
+        # Retourne message d'erreur
+    return int(a) + int(b)  # Retourne la somme de a et b
+
+# Doctest:
+
+add_integer(1, 2)
+add_integer(100, -2)
