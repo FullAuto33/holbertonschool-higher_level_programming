76d47c3365768f6f6b48e77ef8b19fd1bfeb9f95
root <djelloulrouba@yahoo.com>
2025-05-16 08:24:34 +0000

    Script update with docstring in function and avant la function

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+""" Fonction qui additionne deux entiers
+    et retourne la somme
+    si l'un des arguments n'est pas un entier
+    retourne une exception
+    sinon convertit les arguments en entiers et retourne la somme"""
 
 def add_integer(a, b=98):
-    """ Retourne la somme de deux entier"""
+    """ Retourne la somme de deux entier
+        Argument:
+        a: premier entier, b: second entier"""
     if type(a) not in [int, float]:  # Si a n'est pas un entier
         raise TypeError("a must be an integer")
         # Retourne message d'erreur
