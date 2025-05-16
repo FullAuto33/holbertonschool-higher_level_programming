1c55d5dd12294d38fad2d7f4ba90be8293fc9a53
root <you@example.com>
2025-05-16 14:36:27 +0000

    Script test unitaire of matrix

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -4,19 +4,19 @@
 
 def matrix_divided(matrix, div):
     """Divides all elements of a matrix by a number"""
+    messageerreur = "matrix must be a matrix (list of lists) of integers/floats"
+    # message d'erreur
     if not isinstance(matrix, list):
         # verifie si c'est une liste de listes
-        raise TypeError("matrix must be a matrix\
-        (list of lists) of integers/floats")
+        raise TypeError(messageerreur)
         # Retourne typeError si ce n'est pas une liste de listes
     if not all(isinstance(row, list) for row in matrix):
         # verifie si chaque element de la matrice est une liste
-        raise TypeError("matrix must be a \
-        matrix (list of lists) of integers/floats")
+        raise TypeError(messageerreur)
         # Retourne typeError si ce n'est pas une liste de listes
     if not all(isinstance(num, (int, float)) for row in matrix for num in row):
         # verifie si c'est une liste de listes d'entiers ou de flottants
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(messageerreur)
         # Retourne typeError si c'est pas entier ou flottant
     if div == 0:  # Si div est égal à 0
         raise ZeroDivisionError("division by zero")
