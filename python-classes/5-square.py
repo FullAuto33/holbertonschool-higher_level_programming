6c134ee3837569115cfad5fb0456fd619c39031c
root <djelloulrouba@yahoo.com>
2025-05-20 09:44:04 +0000

    Add dunction to print # to square

---
+++ python-classes/5-square.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+"""Class Square that defines a square"""
+
+
+class Square:
+    """Class Square that defines a square"""
+
+    def __init__(self, size=0):
+        """Init size attribute"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.size = size
+        pass
+
+    def area(self):
+        """Returns current square area"""
+        return self.size * self.size
+        pass
+
+    def my_print(self):
+        """Prints in stdout the square with the character #"""
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.size):
+                for j in range(self.size):
+                    print("#", end="")
+                print()
+        pass
+
+    @property
+    def size(self):
+        """Retrieve for size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for size"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+        pass
