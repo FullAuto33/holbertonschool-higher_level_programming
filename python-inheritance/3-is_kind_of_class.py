083ecbc149fe601c61858502245abd0fcce4367f
root <djelloulrouba@ayhoo.com>
2025-05-27 08:28:22 +0000

    Script return the object is an instance of class

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Same class or inherit from"""
+
+
+def is_kind_of_class(obj, a_class):
+    """ function that returns True if the object is an instance of"""
+    return isinstance(obj, a_class)
+    # Retourne Vrai si l'objet est une instance de la classe
