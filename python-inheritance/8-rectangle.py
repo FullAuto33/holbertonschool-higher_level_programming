60460c96a9a28fb14244e98dac338c7862a03a95
root <djelloulrouba@yahoo.com>
2025-05-29 10:37:47 +0000

    Script class rectangle inerith with class basegeomtry

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Rectangle"""
+
+
+class BaseGeometry:
+    """BaseGeometry class with area method."""
+    def area(self):
+        """Raise exception if area is not implemented."""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validate if value is an int greater 0"""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
+
+class Rectangle(BaseGeometry):
+    """Rectangle class inherit from BaseGeometry."""
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
