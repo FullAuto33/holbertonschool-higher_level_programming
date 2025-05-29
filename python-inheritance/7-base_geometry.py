93bfb43b347deab7752fa4af2ccd27c4e8cde2a6
root <djelloulrouba@yahoo.com>
2025-05-29 10:07:35 +0000

    Script base geomtry to verify is value in an int and greather of 0

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
-"""
+"""Integer validator"""
+
+
+class BaseGeometry:
+    """BaseGeometry class with area method."""
+    def area(self):
+        """Raise exception if area is not implemented."""
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """Validate if value is an int greater 0"""
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
