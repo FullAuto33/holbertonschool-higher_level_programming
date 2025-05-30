5e3c9dd9f8b1427ff0ec1dfa094b61ac7d3a9257
root <djelloulrouba@yahoo.com>
2025-05-30 09:24:00 +0000

    Script flying fish

---
+++ python-abc/task_04_flyingfish.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""The Enigmatic FlyingFish - Exploring Multiple Inheritance"""
+
+
+class Fish:
+    """Fish class"""
+
+    def swim(self):
+        """Simulate swimming"""
+        print("The fish is swimming")
+
+    def habitat(self):
+        """return habtitat fish"""
+        print("The fish lives in water")
+
+
+class Bird:
+    """Bird class"""
+
+    def fly(self):
+        """Simulate flying"""
+        print("The bird is flying")
+
+    def habitat(self):
+        """return habitat bird"""
+        print("The bird lives in the sky")
+
+
+class FlyingFish(Fish, Bird):
+    """FlyingFish class inheriting from Fish and Bird"""
+
+    def swim(self):
+        """Override swim method"""
+        print("The flying fish is swimming!")
+
+    def fly(self):
+        """Override fly method"""
+        print("The flying fish is flying!")
+
+    def habitat(self):
+        """Override habitat method"""
+        print("The flying fish lives in both water and sky!")
+
+
+if __name__ == "__main__":
+    flying_fish = FlyingFish()
+    flying_fish.swim()
+    flying_fish.fly()
+    flying_fish.habitat()
