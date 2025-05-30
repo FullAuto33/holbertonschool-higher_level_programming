dced8d0d28f7a3096c8a84a394a4379f06aa9389
root <djelloulrouba@yahoo.com>
2025-05-30 09:11:20 +0000

    Add in the end of file the test

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -21,3 +21,15 @@ class CountedIterator:
     def __iter__(self):
         """Return self as an iterator"""
         return self
+
+
+if __name__ == "__main__":
+    data = [1, 2, 3, 4]
+    counted_iter = CountedIterator(data)
+
+    try:
+        while True:
+            item = next(counted_iter)
+            print(f"Got {item}, total {counted_iter.get_count()} items iterated.")
+    except StopIteration:
+        print("No more items.")
