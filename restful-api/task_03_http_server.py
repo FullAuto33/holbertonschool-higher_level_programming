ef51cc910b277cb99ef4afb6ee023a3ac707b7e4
root <djelloulrouba@yahoo.com>
2025-06-13 04:25:28 +0000

    Request create a simple server for get add run APIs

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,35 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+class SimpleHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+
+        elif self.path == "/data":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            response = {"name": "John", "age": 30, "city": "New York"}
+            self.wfile.write(json.dumps(response).encode())
+
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps({"status": "OK"}).encode())
+
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps({"error": "Endpoint not found"}).encode())
+
+def run():
+    server_address = ("", 8000)
+    httpd = HTTPServer(server_address, SimpleHandler)
+    print("Server running at http://localhost:8000")
+    httpd.serve_forever()
