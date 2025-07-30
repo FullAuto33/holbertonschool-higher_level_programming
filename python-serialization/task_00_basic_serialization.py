cfa0a28e69ef78b052bbf6267aa3e711a0ce82a3
root <djelloulrouba@yahoo.com>
2025-07-30 05:37:23 +0000

    Script serialization basic with json

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Description module serialization basic"""
+
+import json
+
+
+def serialize_to_json_file(my_obj, filename):
+    """Ecrit un objet Python dans un fichier JSON. serialization"""
+    with open(filename, "w") as file:
+        # Ouvre le fichier en mode écriture
+        json.dump(my_obj, file)
+        # Sérialise l'objet Python en JSON et l'écrit dans le fichier
+
+
+def load_and_deserialize(filename):
+    """Lit un fichier JSON et le désérialise en objet Python."""
+    with open(filename, "r") as file:
+        # Ouvre le fichier en mode lecture
+        return json.load(file)
+        # Désérialise le contenu JSON en objet Python et le retourne
