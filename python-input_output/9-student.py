17fefacfa5a742b78b8505e4e162f06c7ff34ba8
root <djelloulrouba@yahoo.com>
2025-07-30 05:04:36 +0000

    Fix problem to pycodestyle

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -7,10 +7,10 @@ class Student:
 
     def __init__(self, first_name, last_name, age):
         """Descriptiojn for the initialization method."""
-        self.first_name = first_name #Attribue la valeur prénom à l’objet
-        self.last_name = last_name #Attribue la valeur nom à l’objet
-        self.age = age #Attribue la valeur âge à l’objet
+        self.first_name = first_name  # Attribue la valeur prénom à l’objet
+        self.last_name = last_name  # Attribue la valeur nom à l’objet
+        self.age = age  # Attribue la valeur âge à l’objet
 
     def to_json(self):
         """Returns the dictionary representation of the Student."""
-        return self.__dict__ # Retourne le dictionnaire de tout les objets
+        return self.__dict__  # Retourne le dictionnaire de tout les objets
