e521c9b3e79abc77f617b91e352adcae7e3796f1
root <djelloulrouba@yahoo.com>
2025-07-30 08:24:58 +0000

    Script create second table and add values

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,16 @@
+-- creates a table second_table in the database hbtn_0c_0 and multiples rows
+CREATE TABLE if not exists second_table (
+    id INT,
+    name VARCHAR(255),
+    score INT
+    );
+
+DELETE FROM second_table;
+
+INSERT INTO second_table (id, name, score) VALUES
+(1, "John", 10),
+(2, "Alex", 3),
+(3, "Bob", 14),
+(4, "George", 8),
+
+    
