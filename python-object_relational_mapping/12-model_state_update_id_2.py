392a9485469339b1bf2d3c09f6d0bd9e704b49f1
root <djelloulrouba@ayhoo.com>
2025-08-01 10:12:56 +0000

    Script change name in database

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -16,9 +16,7 @@ if __name__ == "__main__":
     session = Session()
 
     state = session.query(State).filter(State.id == 2).first()
-    if state:
-        state.name = 'New Mexico'
-        session.commit()
-
+    state.name = 'New Mexico'
     session.commit()
+
     session.close()
