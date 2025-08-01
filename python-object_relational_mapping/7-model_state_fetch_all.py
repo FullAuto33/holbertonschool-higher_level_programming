a097d5a96b4f881c43d9212266d3ec8c1b2fbfb3
root <djelloulrouba@ayhoo.com>
2025-08-01 09:26:15 +0000

    document the module add model state with ibject

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-"""
-lists all State objects from the database
-"""
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+"""lists all State objects from the database"""
+
 import sys
 from model_state import Base, State
+from sqlalchemy import create_engine, select
+from sqlalchemy.orm import sessionmaker
+
 
 if __name__ == "__main__":
-    """
-    Connects to the database and lists all State objects,
-    """
+    """ lists all State objects from the database hbtn_0e_6_usa """
     engine = create_engine(
         'mysql+mysqldb://{}:{}@localhost/{}'.format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        ),
+            sys.argv[1], sys.argv[2], sys.argv[3]),
         pool_pre_ping=True
     )
-
     Session = sessionmaker(bind=engine)
     session = Session()
 
