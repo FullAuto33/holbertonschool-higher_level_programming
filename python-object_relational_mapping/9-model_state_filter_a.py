b32db23a76c14f4b41eceeec3ee0f63babd7d48c
root <djelloulrouba@ayhoo.com>
2025-08-01 09:59:18 +0000

    Script print object contains a in database and add indeteation

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -19,6 +19,6 @@ if __name__ == "__main__":
         session.query(State)
         .filter(State.name.like('%a%'))
         .order_by(State.id):
-        print("{}: {}".format(state.id, state.name)))
+            print("{}: {}".format(state.id, state.name)))
 
     session.close()
