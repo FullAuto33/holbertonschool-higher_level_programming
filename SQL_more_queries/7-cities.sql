49de5e4966220b7d5e6e4703de37ce73bcaa97b2
root <djelloulrouba@ayhoo.com>
2025-08-01 07:29:32 +0000

    Fix oroblem to print the good output

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -7,7 +7,7 @@ CREATE TABLE IF NOT EXISTS states (
 );
 CREATE TABLE IF NOT EXISTS cities (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(256) NOT NULL,
     state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
     FOREIGN KEY (state_id) REFERENCES states(id)
 );
