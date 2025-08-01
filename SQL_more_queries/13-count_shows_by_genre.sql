94c8eb332f9b139a7e141bcc863e4d0abcde65a4
root <djelloulrouba@ayhoo.com>
2025-08-01 08:01:02 +0000

    Script tv genre name is correct fix problem to name of table

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- lists all genres from hbtn_0d_tvshows and displays the number of shows linked to each
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id) AS number_of_shows FROM tv_genres
-INNER JOIN tv_shows ON tv_genres.show_id = tv_shows.id
+INNER JOIN tv_shows_genres ON tv_genres.id = tv_shows_genres.id
 GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
