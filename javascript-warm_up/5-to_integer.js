75c2b15e3c43b15c9d90aa5d618872de88b5c60f
root <djelloulrouba@yahoo.com>
2025-08-01 11:29:51 +0000

    Respecter le semistandard

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-const input = process.argv[2]
-const num = parseFloat(input)
+const num = parseInt(process.argv[2]);
+const num = parseFloat(input);
+const intNum = Math.floor(num);
 
 if (isNaN(num)) {
-  console.log('Not a number')
+  console.log('Not a number');
 } else {
-  const intNum = Math.floor(num)
-  console.log('My number: ' + intNum)
+  console.log('My number: ' + intNum);
 }
