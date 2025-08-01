1e619a790ea8b831315d211fd2460cf6650f5d14
root <djelloulrouba@ayhoo.com>
2025-08-01 09:22:07 +0000

    document the module

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,9 +1,11 @@
 #!/usr/bin/python3
+
 """lists all State objects from the database"""
-import sys
-from model_state import Base, State
-from sqlalchemy import create_engine, select
+
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+import sys
 
 
 if __name__ == "__main__":
