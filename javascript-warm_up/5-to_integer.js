eead3dc46231e9eb2fe1ff69491558b049e00916
root <djelloulrouba@yahoo.com>
2025-08-01 11:05:51 +0000

    Script pirnt numbher argumnt if this is number

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-
-const num = parseInt(process.argv[2]);
+const input = process.argv[2];
+const num = parseFloat(input);
 
 if (isNaN(num)) {
-  console.log('Not a number');
+    console.log('Not a number');
 }
-
 else {
-  console.log('My number: ' + num);
+    const intNum = Math.floor(num);
+    console.log('My number: ' + intNum);
 }
