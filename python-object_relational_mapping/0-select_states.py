77a98228246df67f4bd8abc1bfb910b3f6dae82b
root <djelloulrouba@ayhoo.com>
2025-08-01 08:31:34 +0000

    Script lsit all state with tuple id name in database from python

---
+++ python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" lists all states from the database hbtn_0e_0_usa"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    # Connect to the database
+    db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database)
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
